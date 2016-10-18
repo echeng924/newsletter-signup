c7186b0b88614748d0dcda5610cf77de9055fdc1
Elizabeth Cheng <echeng924@gmail.com>
2016-10-18 19:53:55 -0400

    updated webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: path.join(__dirname, 'client', 'index.jsx'),
+  output: {
+    path: path.join(__dirname, 'dist', 'js'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react'],
+        },
+      },
+    ],
+  },
+};
