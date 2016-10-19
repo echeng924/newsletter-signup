10d302e1eee282273b4806f02dcc51c78972d9d4
Elizabeth Cheng <echeng924@gmail.com>
2016-10-19 19:09:42 -0400

    add member form component

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import MemberForm from './memberForm.jsx';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         Render app
+        <MemberForm />
       </div>
     );
   }
---
+++ client/components/memberForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MemberForm extends React.Component {
+  render() {
+    return (
+      <div id="signup-form">
+        A form will live here..
+      </div>
+    );
+  };
+}
+
+export default MemberForm;
