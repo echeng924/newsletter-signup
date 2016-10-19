0c65414a531520f8d5f684d48ef33ad140ee4636
Elizabeth Cheng <echeng924@gmail.com>
2016-10-19 16:22:24 -0400

    database structure and env file

---
+++ migrations/migrations.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE newsletter_signups;
+
+\c newsletter_signups
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR NOT NULL,
+  last_name VARCHAR NOT NULL,
+  email VARCHAR NOT NULL,
+  zipcode INTEGER NOT NULL,
+);
